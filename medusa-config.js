ad0de5847e976b444cad7cc94a71589967b18b41
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-12-22 15:44:35 -0500

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -1,21 +1,21 @@
 const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = "";
-switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
-  default:
-    ENV_FILE_NAME = ".env";
-    break;
-}
+let ENV_FILE_NAME = ".env";
+//switch (process.env.NODE_ENV) {
+// case "production":
+//    ENV_FILE_NAME = ".env.production";
+//   break;
+// case "staging":
+//  ENV_FILE_NAME = ".env.staging";
+//   break;
+// case "test":
+//   ENV_FILE_NAME = ".env.test";
+//   break;
+// case "development":
+// default:
+//   ENV_FILE_NAME = ".env";
+//   break;
+//}
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
@@ -27,20 +27,13 @@ const ADMIN_CORS = process.env.ADMIN_CORS || "/http://*/";
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "/http://*/";
 
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+const DATABASE_URL = process.env.DATABASE_URL || "";
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || "";
 
 const plugins = [
   `medusa-fulfillment-manual`,
   /**`medusa-payment-manual`,*/
-  {
-    resolve: `@medusajs/file-local`,
-    options: {
-      upload_dir: "uploads"
-    }
-  },
   {
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
@@ -63,7 +56,7 @@ const plugins = [
   {
     resolve: `@medusajs/file-local`,
     options: {
-      upload_dir: process.env.FILE_UPLOAD_DIR || "uploads/images",
+      upload_dir: process.env.FILE_UPLOAD_DIR || "",
       backend_url: process.env.FILE_BACKEND_URL
     }
   },
