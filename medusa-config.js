39469025fe3bd9878cef52f4da628fa1831755b2
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-12-22 22:20:42 -0500

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -1,21 +1,21 @@
 const dotenv = require("dotenv");
 
 let ENV_FILE_NAME = ".env";
-//switch (process.env.NODE_ENV) {
-// case "production":
-//    ENV_FILE_NAME = ".env.production";
-//   break;
-// case "staging":
-//  ENV_FILE_NAME = ".env.staging";
-//   break;
-// case "test":
-//   ENV_FILE_NAME = ".env.test";
-//   break;
-// case "development":
-// default:
-//   ENV_FILE_NAME = ".env";
-//   break;
-//}
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
@@ -33,7 +33,7 @@ const REDIS_URL = process.env.REDIS_URL || "";
 
 const plugins = [
   `medusa-fulfillment-manual`,
-  /**`medusa-payment-manual`,*/
+  `medusa-payment-manual`,
   {
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
